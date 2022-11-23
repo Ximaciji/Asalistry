a33e5360cf5060f71fba08368396dd2dda046719
Ximaciji <45185343+Ximaciji@users.noreply.github.com>
2022-11-23 10:32:53 -0500

    Add files via upload

---
+++ Asalinism/PlannerStuff/CelestialNinjaGear.cs
@@ -0,0 +1,23 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Asalinism.PlannerStuff
+{
+	public class CelestialNinjaGear : ModItem
+	{
+		public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Celestial Ninja Gear");
+			Tooltip.SetDefault("Celestial Shell + Master Ninja Gear");
+		}
+
+		public override void SetDefaults() {
+			Item.width = 26;
+			Item.height = 32;
+			Item.maxStack = 1;
+			Item.value = Item.sellPrice(gold: 5);
+			Item.accessory = true;
+			Item.rare = ItemRarityID.Red;
+		}
+	}
+}
---
+++ Asalinism/PlannerStuff/TerramphibianBoots.cs
@@ -0,0 +1,28 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Asalinism.PlannerStuff
+{
+	[AutoloadEquip(EquipType.Shoes)]
+	public class TerramphibianBoots : ModItem
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Terramphibian Boots");
+			Tooltip.SetDefault("Terraspark + Frog Leg + Flippers");
+		}
+
+		public override void SetDefaults() {
+			Item.width = 28; 
+			Item.height = 24;
+			Item.accessory = true; // Makes this item an accessory.
+			Item.rare = ItemRarityID.Blue; 
+			Item.value = Item.sellPrice(gold: 1); // Sets the item sell price to one gold coin.
+		}
+
+		public override void UpdateAccessory(Player player, bool hideVisual) {
+			player.accRunSpeed = 6f; // The player's maximum run speed with accessories
+			player.moveSpeed += 0.05f; // The acceleration multiplier of the player's movement speed
+		}
+	}
+}
